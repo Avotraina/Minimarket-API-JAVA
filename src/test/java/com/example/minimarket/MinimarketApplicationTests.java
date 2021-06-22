533ae97dc6efe11e15a13f8dac78de4eff2250f9
Avotraina <avotrainarabe@gmail.com>
2021-06-22 10:22:59 +0300

    error

---
+++ src/test/java/com/example/minimarket/MinimarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
